c29f67db7b3a092870553d14e15d4a16596e37e1
Andres Hense <andreshense@gmail.com>
2020-12-21 00:34:08 -0300

    arregla muchas cosas

--- public/js/firebaseBuscador.js
+++ public/js/firebaseBuscador.js
@@ -2,7 +2,42 @@
 	
 	  museumListRef = database.ref("museum");
 	  
-	  
+	  function changeCardsInMuseo(){
+		let datoSearcho= document.getElementById("buscador").value;
+		window.location='../../index.html';
+	  	isChanged=true;
+	  	limpiaCards(museumListRef);
+	  	
+	  	museumListRef.on("value", function (snapshot) {
+	  	snapshot.forEach(function (childSnapshot) {
+	  		var data = childSnapshot.val();
+	  		
+	
+	  		var caracteristicaAbreviada= data.descripcion.substr(0,65);
+	  		var tituloAbreviado = data.name.substr(0,31);
+	  		var estadoDelMuseo = estadoMuseoActual(data);
+	  		
+	  		
+	  		
+	  		//let datoSearcho= document.getElementById("buscador").value;
+	  		
+			let regexSearcho= RegExp(datoSearcho,"i");
+			console.log(datoSearcho);
+	  		console.log(regexSearcho.test(data.name));
+			
+	  		if(regexSearcho.test(data.name)){
+	  			let museoExtras={
+	  				_tituloAbreviado: tituloAbreviado,
+	  				_caracteristicaAbreviada: caracteristicaAbreviada,
+	  				_estadoDelMuseo: estadoDelMuseo,
+	  			}
+				creaCard(data,museoExtras,'elem');
+	  		}
+	  	 });
+	  })
+	  	
+	  }
+
 	  function changeCards(){
 	  	isChanged=true;
 	  	limpiaCards(museumListRef);
